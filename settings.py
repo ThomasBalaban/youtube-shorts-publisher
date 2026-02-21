53e8408276384ddbd91fa37a119ead5369d96da4
ThomasBalaban <balabaninteractive@gmail.com>
2026-02-21 16:41:18 -0500

    asdf

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from gemjam import GEMJAM
 
 PROCESS_SINGLE_VIDEO = False  # Set to False to scrape all drafts
-ENABLE_SCRAPING_MODE = True # If this is True, the bot will ONLY scrape  videos and output to JSON.
+ENABLE_SCRAPING_MODE = False # If this is True, the bot will ONLY scrape  videos and output to JSON.
 ENABLE_ANALYSIS_MODE = True  # Run the Analyzer
 VIDEOS_TO_PROCESS_COUNT = 50  # How many videos to schedule in a row
 TEST_MODE = False # Will only process a single video, will not save 
